ca7d8001cea9a5e02a1267e4071c3b6df4b1ce03
unknown <chokribenmahjoub>
2023-02-07 13:06:21 +0100

    qr code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import html2canvas from "html2canvas";
+import { QRCodeCanvas } from "qrcode.react";
+import React, { useState } from "react";
+import { AiFillCopy, AiOutlineDownload } from "react-icons/ai";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [qr, setqr] = useState("");
+    const [url, seturl] = useState("");
+    const QrCodeDownload = async() => {
+        const canvas = await (
+            await html2canvas(document.getElementById("canvas"))
+        ).toDataURL();
+
+        if (canvas) {
+            setqr(canvas);
+            const a = document.createElement("a");
+            a.download = "QrCode.png";
+            a.href = canvas;
+            document.body.appendChild(a);
+            a.click();
+            document.body.removeChild(a);
+        }
+    };
+
+    const QrCodeCopy = () => {
+        navigator.clipboard.writeText(qr);
+    };
+    return ( <
+        div className = "container mx-auto w-[320px]" >
+        <
+        div class = "mb-4" >
+        <
+        p className = "text-2xl" > Generate QrCode < /p> < /
+        div > <
+        div class = "mb-4" >
+        <
+        label class = "block text-gray-700 text-sm  mb-2" > Write something < /label> <
+        input class = "shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        type = "text"
+        onChange = {
+            (e) => seturl(e.target.value)
+        }
+        /> < /
+        div >
+
+        <
+        div id = "canvas"
+        className = "border p-2 relative" >
+        <
+        QRCodeCanvas value = { url }
+        size = { 300 }
+        bgColor = { "#ffffff" }
+        fgColor = { "#0a75ad" }
+        level = { "H" }
+        includeMargin = { false }
+        imageSettings = {
+            {
+                src: "/chokri.jpg",
+                x: undefined,
+                y: undefined,
+                height: 80,
+                width: 40,
+                excavate: true,
+            }
+        }
+        /> < /
+        div > <
+        div className = "flex w-[300px] mt-4 p-4 space-x-2 items-center justify-center" >
+        <
+        button onClick = {
+            () => QrCodeDownload()
+        }
+        class = "flex items-center justify-between bg-transparent hover:bg-[#0a75ad] text-[#0a75ad] font-semibold hover:text-white py-2 px-4 border border-[#0a75ad] hover:border-transparent rounded" >
+        <
+        AiOutlineDownload / >
+        Download <
+        /button>
+
+        <
+        button onClick = {
+            () => QrCodeCopy()
+        }
+        class = "flex items-center  justify-between bg-transparent hover:bg-[#0a75ad] text-[#0a75ad] font-semibold hover:text-white py-2 px-4 border border-[#0a75ad] hover:border-transparent rounded" >
+        <
+        AiFillCopy / >
+        Copy <
+        /button> < /
+        div > <
+        /div>
+    );
 }
 
 export default App;
